f194b1fcd3afb81cfc3254ca3b4a9a1ed3f92b32
Amit Biswas <amit.142biswas@gmail.com>
2024-04-20 22:36:13 +0530

    will do later

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -14,7 +14,36 @@ export async function GET(request: Request){
         const queryParams = {
             username: searchParams.get('username')
         }
-                
+
+        const result = UsernameQuerySchema.safeParse(queryParams)
+        console.log(result)
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || []
+            return Response.json({
+                success: false,
+                message:
+                usernameErrors?.length > 0
+                  ? usernameErrors.join(', ')
+                  : 'Invalid query parameters',
+            }, {status: 400})
+        }
+        
+        const {username} = result.data
+
+        const existingVerifiedUser = await UserModel.findOne({username, isVerfied: true})
+
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: 'Username is already taken',
+            }, {status: 400})                        
+        }
+
+        return Response.json({
+            success: true,
+            message: 'Username is unique',
+        }, {status: 400})
+
     } catch (error) {
         console.error("Error checking username", error)
         return Response.json(
