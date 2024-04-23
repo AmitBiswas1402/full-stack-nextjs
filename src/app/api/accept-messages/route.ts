8ab6c251c106f474d362da7bf0a444e0ed8b7948
Amit Biswas <amit.142biswas@gmail.com>
2024-04-23 21:43:35 +0530

    accept messages route made

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -22,4 +22,88 @@ export async function POST(request: Request) {
 
   const userId = user._id
   const {acceptMessages} = await request.json()
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      {isAcceptingMessage: acceptMessages},
+      {new: true}
+    )
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update user status to accept",
+        },
+        { status: 401 }
+      );
+    }
+    
+    return Response.json(
+      {
+        success: true,
+        message: "Message acceptance status updated sucessfully",
+        updatedUser
+      },
+      { status: 200 }
+    );
+
+  } catch (error) {
+    console.log("Failed to update user status to accept")
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to update user status to accept",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User
+
+  if (!session || !session) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id
+
+  try {
+    const foundUser = await UserModel.findById(userId)
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      )
+    }
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    )
+  } catch (error) {
+    console.log("Failed to update user status to accept")
+    return Response.json(
+      {
+        success: false,
+        message: "Error in getting message acceptance status",
+      },
+      { status: 500 }
+    );    
+  }
 }
