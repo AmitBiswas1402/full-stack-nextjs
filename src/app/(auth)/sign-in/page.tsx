f707b56a30d3c7dc75a041beea06a60f88dace8a
Amit Biswas <amit.142biswas@gmail.com>
2024-04-28 22:34:59 +0530

    frontend started

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,13 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import React from 'react'
+import { useForm } from'react-hook-form'
+import * as z  from 'zod'
+import Link from 'next/link'
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+function page() {
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-blue-600 px-4 py-2 m-4 rounded-md" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>page</div>
   )
 }
+
+export default page
