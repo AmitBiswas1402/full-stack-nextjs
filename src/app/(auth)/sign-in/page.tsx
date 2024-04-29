eb016a1f262f2a5107b82da6bd8f5c81adc170fa
Amit Biswas <amit.142biswas@gmail.com>
2024-04-29 22:46:33 +0530

    nothin ;(((

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 import { useForm } from'react-hook-form'
 import * as z  from 'zod'
 import Link from 'next/link'
+import {useState} from 'react'
 
 function page() {
+  const [username, setUsername] = useState('')
+
   return (
     <div>page</div>
   )
